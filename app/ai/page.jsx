54bcc2047372f6522b4865ece75ae699071797c5
Bhawneet Singh <bsingh_be22@thapar.edu>
2025-07-26 16:56:17 +0530

    fixed toggle

--- app/ai/page.jsx
+++ app/ai/page.jsx
@@ -81,6 +81,7 @@ function Page()
           )}
         </div>
       )}
+      </div>
     </div>
   );
 }
--- app/category/[category]/page.jsx
+++ app/category/[category]/page.jsx
@@ -86,9 +86,7 @@ function Page({ params })
             </div>
           ))}
       </div>
-
-      {/* Theme Toggle Button */}
-      <ThemeToggle />
+      </div>
     </div>
   );
 }
